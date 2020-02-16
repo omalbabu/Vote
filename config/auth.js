f1e0cf844fee00ff7da8e2e3d3394445ba5403d9
omal <omalsoware@gmail.com>
2020-02-16 23:21:22 +0530

    commit Dashboard

---
+++ config/auth.js
@@ -0,0 +1,11 @@
+module.exports={
+    ensureAuthenticated:(req,res,next)=>{
+        if(req.isAuthenticated())
+        {
+            return next();
+
+        }
+        req.flash('error_msg','Please log in.');
+        res.redirect('/users/login');
+    }
+}
